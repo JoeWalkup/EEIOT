8dcf0fffbb711070cacc3f87917dae0d7ca2dd8e
J <161602796+JoeWalkup@users.noreply.github.com>
2024-10-23 15:11:10 -0700

    Update main.cpp

--- 2sensordatabase/main.cpp
+++ 2sensordatabase/main.cpp
@@ -1,15 +1,54 @@
+/*
+ * ----------------------------------------------
+ * Project/Program Name : <2sensordatabase>
+ * File Name            : <main.ccp>
+ * Author               : <Joseph Walkup>
+ * Date                 : <23/10/24>
+ * Version              : <2>
+ * 
+ * Purpose:
+ *    <Send temp and humidity data from sensors to database>
+ * 
+ * Inputs:
+ *    <Timezone
+ *    Temperature
+ *    Humidity>
+ * 
+ * Outputs:
+ *    <"Serial Outputs">
+ * 
+ * Example Application:
+ *    <When button is pressed the current time, temperature, and humididty is sent to a database>
+ * 
+ * Dependencies:
+ *    <Arduino, ESP8266WiFi, ESP8266HTTPClient, Adafruit Sensor DHT library, ArduinoJson>
+ * 
+ * 
+ * ---------------------------------------------------------------------------
+ */
 
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-#include <sendrequest.h> // required header file
+#include <sendrequest.h> 
+#include <sensortime.h>
+#include <DHT.h> 
+#include <Adafruit_Sensor.h>
 //#include <WiFiClientSecure.h> // not needed
 
 const char* ssid     = "SEJW"; // must be exactly as it apears - case sensitive
 const char* password = "bob12345";
-int i;
-String url     = "https://josephwalkup.net/createdatabase/add.php";
-String data1   = "?node_name=node-1&time_received=2022-10-01%2011:00:00&temperature=70&humidity=70"; // currently empty
+
+const char* timeZones[] = {
+  "America/New_York",   // Eastern
+  "America/Chicago",    // Central
+  "America/Denver",     // Mountain
+  "America/Los_Angeles",// Pacific
+  "America/Anchorage",  // Alaska
+  "Pacific/Honolulu",   // Hawaii-Aleutian
+  "America/Puerto_Rico" // Atlantic
+};
+String timeZone = "America/Los_Angeles";
 
 void setup () {
   Serial.begin(9600);
@@ -26,35 +65,58 @@ void setup () {
   }
   // Details of the connection
    connectionDetails();
+     dht.begin();
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+   Serial.println("Select Your Time Zone (The default time is PT):");
+  Serial.println("1- Eastern Time Zone (ET) - New York City, NY");
+  Serial.println("2- Central Time Zone (CT) - Chicago, IL");
+  Serial.println("3- Mountain Time Zone (MT) - Denver, CO");
+  Serial.println("4- Pacific Time Zone (PT) - Los Angeles, CA");
+  Serial.println("5- Alaska Time Zone (AKT) - Anchorage, AK");
+  Serial.println("6- Hawaii-Aleutian Time Zone (HAT) - Honolulu, HI");
+  Serial.println("7- Atlantic Time Zone (AT) - San Juan, PR");
+  Serial.print("Enter a number between 1-7 to select your time zone: ");
+
+  while (!Serial.available()) {
+    delay(200);
+  }
+
+  // Read the input
+  int selection = Serial.parseInt();
+  // Display the input
+  Serial.println(selection);
+
+  // Check if input is valid
+  if (selection >= 1 && selection <= 7) {
+    // Set timeZone based on user input 
+    timeZone = timeZones[selection - 1];
+    Serial.print("Time zone set to: ");
+    Serial.println(timeZone);
+  } else {
+    // Set timezone to default if input is invalid
+    Serial.println("Invalid selection. Using default time zone (Pacific Time).");
+  }
+
 }
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
-    WiFiClientSecure client;
-    client.setInsecure();
-    HTTPClient https;
+  if (digitalRead(BUTTON_PIN) == LOW) {
+    Serial.println("button pressed");
+    delay(150);
 
-    // For testing only
-
-    String fullUrl = url + data1; // preparing the full URL
-    Serial.println("Requesting: --> " + fullUrl);
-    i = 0;
-    while (i == 0) {
-    if (https.begin(client, fullUrl)) { // start the connection 1=started / 0=failed
-
-      int httpCode = https.GET(); // choose GET or POST method
-      //int httpCode = https.POST(fullUrl); // need to include URL
-      
-      Serial.println("Response code <--: " + String(httpCode)); // print response code: e.g.,:200
-      if (httpCode == 200) {
-        //Serial.println(https.getString()); // this is the content of the get request received
-      i = 1;
-      }
+    float temp = readtemperature();
+    float humid = readhumidity();
+  if (isnan(temp) || isnan(humid)) {
+      Serial.println("sensor error");
+    } 
     else {
-        Serial.printf("[HTTPS] Unable to connect\n");
-    }}
-          https.end(); // end of request
-  }
-  delay(5000); // delay between each REQUEST to the server
-
-}}
-
+      String currenttime = getCurrentTime(timeZone);
+      transmitdata(temp, humid,currenttime);
+      Serial.println();
+    }
+    while (digitalRead(BUTTON_PIN) == LOW) {
+      yield();}
+    
+    
+    }
+delay(200);
+}
